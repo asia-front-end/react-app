60bae6b11c80e13efcb135685bc9b2b5b6c547c8
dziedzioj <j.dziedzio@tt.com.pl>
2018-12-06 14:16:00 +0100

    Updates

---
+++ precache-manifest.d86cabf81e22f81196f9704c6e577729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "57141f25268e75f2414a",
+    "url": "/react-app/static/js/main.57141f25.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/react-app/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "57141f25268e75f2414a",
+    "url": "/react-app/static/css/main.14e07049.chunk.css"
+  },
+  {
+    "revision": "b402cdba840688537b1f78b6d873a7f0",
+    "url": "/react-app/index.html"
+  }
+];
